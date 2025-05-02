87b811960563bec696a826b9e5d2f8fb3d984ca9
João Vitor Alves Amaral <am.joao13@gmail.com>
2025-05-02 10:05:35 -0300

    inserção arquivos

---
+++ app.js
@@ -0,0 +1,70 @@
+const itens = [{
+    "id": 1,
+    "titulo": "Como o dólar alto afeta o bolso dos brasileiros",
+    "resumo": "Apesar dos recentes recuos, moeda norte-americana ainda acumula valorização de 12,6% contra o real em 2024",
+    "texto": "<p>Apesar do forte recuo do dólar nos últimos dias, fechando a semana cotado a R$ 5,46, a divisa ainda se mantém no maior patamar desde 2022 — resultado de uma escalada iniciada em abril e que se intensificou em junho.<br> Em 2024 a moeda ainda acumula elevação de 12,6%.<br>Desde viagens ao exterior até o encarecimento de produtos nas gôndolas dos supermercados, o dólar alto deve afetar a economia e o bolso dos brasileiros a curto e médio prazo, segundo economistas ouvidos pela CNN.<br>Confira abaixo como a perda de valor do real ante a principal moeda do globo tende a impactar o cotidiano no Brasil.</p><h1>Viagens mais caras</h1><p>Um dos principais sinais perceptíveis da alta do dólar está no encarecimento das viagens ao exterior.<br>Neste caso, o efeito mais palpável está na perda de valor da moeda brasileira — o que demanda mais reais para compra de produtos ou contratação de serviços cotados em dólar.<br>Em outro aspecto, o custo das passagens também se torna menos acessível, já que grande parte das despesas das companhias aéreas está atrelada à moeda norte-americana.<br>“De forma imediata, vejo efeito que se a pessoa está com viagem marcada ou com algum compromisso de compra de imóvel fora do país, por exemplo, onde se precisa desembolsar o valor em dólar”, explica a economista-chefe do Ouribank, Cristiane Quartaroli.</p><h1>Produtos mais caros no Brasil</h1><p>O encarecimento dos produtos nas lojas e supermercados no Brasil é outro efeito relacionado ao aumento da cotação do câmbio.<br> Por um lado, a importação de bens se torna mais cara diante da disparidade do valor entre o real e a divisa norte-americana. Esse efeito acaba se espalhando por toda a cadeia, visto que mesmo itens produzidos no Brasil usam componentes, insumos ou maquinários estrangeiros.<br>“Mesmo nos grãos, que o Brasil tem certo protagonismo, as cotações também são estabelecidas em dólar, então soja e milho podem ficar mais caros”, explica André Braz, pesquisador do Instituto Brasileiro de Economia da Fundação Getulio Vargas (Ibre/FGV).<br>O aumento do dólar também torna o mercado internacional mais atraente, fazendo com que produtores brasileiros priorizem as exportações ao consumo interno. Com menos oferta disponível, o preço do produto tende a apontar para cima.<br>“Sempre que o real se desvaloriza frente ao dólar, é como se os produtos brasileiros entrassem em promoção e isso pode estimular um volume maior de exportações”, complementa Braz</p>",
+    "tipo": "Curiosidades",
+    "autor": "Isabella Marzolla",
+    "data": "07/07/2024",
+    "banner": "/img/img-educacao/dolar.png",
+    "descricao": "Imagem dolar",
+    "iframe": "https://www.youtube.com/embed/yQf7P6CPZFY"
+},
+{
+    "id": 2,
+    "titulo": "Dicas de como economizar dinheiro no dia a dia",
+    "resumo": "Você sabe como fazer o dinheiro render ao final do mês? Como adaptar sua rotina para economizar todos os dias? Pode parecer difícil entender como economizar dinheiro ganhando pouco, mas sabendo como administrar o que recebe você consegue ir mais longe do que acredita.",
+    "texto": "<h1>Como economizar dinheiro no dia a dia? Dicas para a sua rotina!</h1><br><h2>1. Analise seus hábitos de consumo</h2><br><p>Mantenha um controle financeiro atualizado para entender seus hábitos de consumo. O “conheça a si mesmo” nunca fez tanto sentido quanto repassar a fatura do cartão no fim do mês.<br>Tome um tempo para registrar todas as suas despesas e receitas em uma planilha ou aplicativo, classificando-as por tipo e categoria.</p><br><h2>2. Crie metas de economia</h2><p>Estabeleça metas de economia claras e objetivas utilizando a metodologia SMART: específicas, mensuráveis, alcançáveis, relevantes e temporais. Assim, você tem a satisfação de alcançar um objetivo enquanto se sente motivado a economizar.<br>Veja esse exemplo: para viajar para a praia, você precisa economizar R$ 200 por mês ao longo de um ano. Essa meta é específica (R$ 200), mensurável (monitorar mensalmente), alcançável (ajustar gastos), relevante (para a viagem), e temporal (até o fim do ano).</p><br><h2>3. Priorize necessidades sobre desejos e corte o que for desnecessário</h2><p>Priorize suas necessidades e corte gastos desnecessários. Dos cinco serviços de assinatura, quantos você realmente assiste todo dia? E aquela academia, você está indo ou só pagando? <br>Avalie serviços por assinatura, como streaming e revistas, e cancele aqueles que não são usados regularmente. Verifique mensalidades de clubes ou compras programadas que podem ser ajustadas ou eliminadas para economizar mais.</p><h2>4. Negocie tarifas e contas</h2><p>Ligue para serviços como internet, telefone e TV a cabo, e negocie as melhores tarifas. Você pode aproveitar uma promoção ou reduzir pacotes que não está usando na totalidade.<br>Pergunte sobre descontos para pagamentos antecipados ou planos mais econômicos. Muitas vezes é possível reduzir significativamente as despesas com uma simples conversa.</p>",
+    "tipo": "Educação",
+    "autor": "PagBank",
+    "data": "01/11/2024",
+    "banner": "/img/img-educacao/organizacao.jpg",
+    "descricao": "Organização Financeira",
+    "iframe": "https://www.youtube.com/embed/kqFzRpoOPMc"
+}, {
+    "id": 3,
+    "titulo": "O que é inflação e por que ela impacta no seu bolso?",
+    "resumo": "Inflação é um conceito que traduz o aumento generalizado de preços de bens e serviços. Entenda tudo sobre o índice e como ele afeta os seus investimentos",
+    "texto": "<br><h1>O que é inflação?</h1><p>Inflação é um termo da economia usado para designar o aumento generalizado de preços de bens e serviços. Com isso, a inflação representa o aumento do custo e vida e a consequente redução no poder de compra da moeda.</p><br><p>Uma forma simples de perceber a inflação é indo ao supermercado. O preço dos produtos nas prateleiras não são os mesmos praticados 10, 15 ou 20 anos atrás: estão mais caros. A inflação também impacta nos valores de itens maiores, como aluguel e automóveis, ou itens de longo prazo, como a mensalidade de uma faculdade ou pós-graduação.</p><br><p>Esse aumento de preços não é necessariamente ruim ou prejudicial ao consumidor, principalmente quando é controlado, espaçado ao longo do tempo e vem acompanhado de reajustes nos salários-mínimos. Mas pode causar transtornos para consumidores – e para a economia de um país – quando aumenta em uma velocidade maior do que pode ser absorvida.</p><br><h1>Quais são as causas da inflação?</h1><p>Um aumento nos índices de inflação pode ser influenciado por diferentes causas, que podem ser agrupadas em quatro grandes grupos:</p><br><ol><li> Aumento na demanda;</li><li> Aumento, ou pressões, nos custos de produção;</li><li> Inércia inflacionária e expectativas de inflação;</li><li> Aumento de emissão de moeda.</li></ol><br><p>As causas também podem ser divididas em de curto prazo ou um prazo maior, que aumentam de forma contínua durante meses ou até anos.  Dificilmente, no entanto, a causa da inflação será única.</p>",
+    "tipo": "Educação",
+    "autor": "InfoMoney",
+    "data": "6/11/2022 ",
+    "banner": "/img/img-educacao/dinheiro-inflacao.png",
+    "descricao": "Inflação e dinheiro",
+    "iframe": "https://www.youtube.com/embed/A4b7PyZ-DnA"
+},
+{
+    "id": 4,
+    "titulo": "Como fazer testamento? Saiba como criar um do zero",
+    "resumo": "Documento é a forma mais famosa de planejamento sucessório, mas é preciso saber fazê-lo para que tenha validade no futuro",
+    "texto": "<h1>O que é um testamento?</h1><p>Um testamento nada mais é do que uma declaração de vontade da pessoa que o faz (também chamada de “testador”) sobre o que ela gostaria que acontecesse após a sua morte.</p><br><p>Como vimos, ele pode conter aspectos que vão além do patrimônio, desde que não extrapole o que a lei determina. Um exemplo foi o caso da apresentadora Glória Maria, que deixou expresso no testamento quem seriam os tutores legais de suas duas filhas, que tinham 14 e 15 anos quando ela faleceu, em fevereiro de 2023.</p><br><p>Outro caso conhecido e que não foi corretamente divulgado, segundo Viviane, foi de Nélida Pigñón, falecida em dezembro de 2022. Na ocasião, alguns veículos informaram que a escritora havia deixado um apartamento como herança para suas duas cachorrinhas.</p><br><p>“A informação não foi divulgada corretamente, pois animal não pode receber patrimônio. Na verdade, ela deixou uma pessoa como tutora dos cachorros, que ficou responsável pela casa e pelos cuidados com os animais”, explica a advogada.</p><br><p>Viviane também observa que a tutoria (seja de pessoas ou animais) também pode ser expressa em outro tipo de documento, como uma carta de próprio punho, por exemplo. Porém, o testamento sempre tem mais força, principalmente se ele é do tipo público, conforme veremos mais adiante.</p>",
+    "tipo": "Educação",
+    "autor": "InfoMoney",
+    "data": "7/05/2024",
+    "banner": "img/img-educacao/testamento-assinado.jpg",
+    "descricao": "Para que serve um testamento?",
+    "iframe": "https://www.youtube.com/embed/mHpfQLVdonM"
+}
+]
+
+const usuario = [{
+    "id": 1,
+    "usuario": "João",
+    "comentario": "Essa dicas vão me ajudar muito!"
+},
+{
+    "id": 2,
+    "usuario": "Maria",
+    "comentario": "Eu não sabia como isso funcionava!"
+},
+{
+    "id": 3,
+    "usuario": "Marcos",
+    "comentario": "A inflação está muito alta!"
+},{
+    "id": 4,
+    "usuario": "Jonathan",
+    "comentario": "Tenho que fazer meu testamento :("
+}
+]
+
